b8cb21c6e8925b1299dbfd99c852c134854f8679
roihantauhid <roihanntauhidd>
2024-02-28 15:26:15 +0700

    roihan edit loginadmin

--- src/admin/InputArtikel.jsx
+++ src/admin/InputArtikel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const InputArtikel = () => {
-  return <div>Ini Halaman Input Artikel</div>;
+  return <div className="text-gray-600">Ini Halaman Input Artikel</div>;
 };
 
 export default InputArtikel;
--- src/admin/LoginAdmin.jsx
+++ src/admin/LoginAdmin.jsx
@@ -3,7 +3,36 @@ import React from "react";
 const LoginAdmin = () => {
   return (
     <div className="flex justify-center items-center text-center">
-      <div className="w-1/2 h-screen">Kotak1 </div>
+      {/* Section 1 */}
+      <div className="m-auto w-1/2 h-screen flex justify-center items-center font-poppins">
+        <div className=" w-90 h-60 border-solid border-2 border-indigo-600">
+          <form action="">
+            <h1>
+              <span className="text-7xl font-bold">Donor</span>
+              <span className="text-7xl font-bold">.Pdg</span>
+            </h1>
+            <div class="form1">
+              <input type="text" name="email" id="email" placeholder="Email" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div class="check">
+              <input type="checkbox" name="checkbox" id="checkbox" />
+              <label for="">Remember me</label>
+              <a href="">Forgot password?</a>
+            </div>
+            <a href="" class="button">
+              Login
+            </a>
+          </form>
+        </div>
+      </div>
+
+      {/* Section 2 */}
       <div className="w-1/2 h-screen bg-gradient-to-l from-red-500 via-red-500 to-transparent flex justify-center items-center">
         <div className="text text-white text-center justify-center font-poppins italic space-y-2">
           <span className="text-2xl "> "Mari donor</span>
