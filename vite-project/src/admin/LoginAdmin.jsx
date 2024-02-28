45d6ac2730924cf26b19fa478b229ca61783cf0d
roihantauhid <roihanntauhidd>
2024-02-29 03:48:46 +0700

    roihan finis adminlogin

--- vite-project/src/admin/LoginAdmin.jsx
+++ vite-project/src/admin/LoginAdmin.jsx
@@ -5,27 +5,41 @@ const LoginAdmin = () => {
     <div className="flex justify-center items-center text-center">
       {/* Section 1 */}
       <div className="m-auto w-1/2 h-screen flex justify-center items-center font-poppins">
-        <div className=" w-90 h-60 border-solid border-2 border-indigo-600">
+        <div className=" h-120 w-120 p-4 ">
           <form action="">
-            <h1>
-              <span className="text-7xl font-bold">Donor</span>
-              <span className="text-7xl font-bold">.Pdg</span>
+            <h1 className="mb-6">
+              <span className="text-5xl font-bold text-donor">Donor</span>
+              <span className="text-5xl font-bold text-pdg">.Pdg</span>
             </h1>
-            <div class="form1">
-              <input type="text" name="email" id="email" placeholder="Email" />
+            <div className="h-90 w-96">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Email"
+                className="border-solid border-2 border-border rounded-md block w-full h-8 mb-5 p-4"
+              />
               <input
                 type="password"
                 name="password"
                 id="password"
                 placeholder="Password"
+                className="border-solid border-2 border-border rounded-md block w-full h-8 mb-1 p-4"
               />
             </div>
-            <div class="check">
+            <div className="flex">
               <input type="checkbox" name="checkbox" id="checkbox" />
-              <label for="">Remember me</label>
-              <a href="">Forgot password?</a>
+              <label for="" className="p-1">
+                Remember me
+              </label>
+              <a href="" className="ml-auto text-xs mt-2">
+                Forgot password?
+              </a>
             </div>
-            <a href="" class="button">
+            <a
+              href=""
+              className="rounded-md block bg-donor p-2 mt-8 text-white hover:shadow-xl hover:shadow-donor transition duration-300 "
+            >
               Login
             </a>
           </form>
@@ -33,13 +47,13 @@ const LoginAdmin = () => {
       </div>
 
       {/* Section 2 */}
-      <div className="w-1/2 h-screen bg-gradient-to-l from-red-500 via-red-500 to-transparent flex justify-center items-center">
+      <div className="w-1/2 h-screen bg-custom-gradient flex justify-center items-center">
         <div className="text text-white text-center justify-center font-poppins italic space-y-2">
           <span className="text-2xl "> "Mari donor</span>
           <span className="font-bold text-4xl"> mendonor</span>
           <p className="space-y-8">
             <span className="text-2xl "> agar mendonor jadi </span>
-            <span className="font-bold text-4xl">donormen</span>{" "}
+            <span className="font-bold text-4xl">donormen</span>
             <span className="text-2xl ">"</span>
           </p>
           <br />
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        donor: "#C92B3C",
+        pdg: "#3F3F3F",
+        border: "#878787",
+      },
+      backgroundImage: (theme) => ({
+        "custom-gradient":
+          "linear-gradient(to left, rgba(141, 42, 52, 1), rgba(140, 41, 51, 0.72), rgba(138, 40, 50, 0.45), rgba(136, 39, 49, 0))",
+      }),
+    },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
