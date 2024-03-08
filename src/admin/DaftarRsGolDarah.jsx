f3d65cdef25991964d43f477a1808c2ac1607159
KevinJonaAlfarel <keviinalfarel22@gmail.com>
2024-03-08 14:28:31 +0700

    kevinlagi

--- src/admin/DaftarRsGolDarah.jsx
+++ src/admin/DaftarRsGolDarah.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../index.css";
-import Profil from "../image/profil1.jpeg";
+import Profil from '../image/profil1.jpeg';
 import frame from "../component/Frame.png";
 import droplet from "../component/droplet-fill.png";
 import fileearmark from "../component/file-earmark-fill.png";
@@ -18,316 +18,42 @@ import smph from "../component/trash-fill.png";
 import rs2 from "../component/Rectangle 4.png";
 
 const DaftarRsGolDarah = () => {
-  const [clicked, setClicked] = useState(false);
+    const [clicked, setClicked] = useState(false);
 
-  return (
-    <div className="h-screen font-poppins">
-      {/* SECTION HEAD */}
-      <div className="flex h-20 ">
-        <div className="flex ">
-          <div className="p-4">
-            <span className="text-[26px] font-bold text-donor cursor-pointer">
-              Donor
-            </span>
-            <span className="text-[26px] font-bold text-pdg cursor-pointer">
-              .Pdg
-            </span>
-          </div>
-          <div className="flex pl-[420px] ml-[90px]">
-            <div className="w-[600px] text-right p-4">
-              <div>
-                <span className="font-poppins font-semibold w-[600px]">
-                  Dr.fadel Muhammad
-                </span>
-              </div>
-              <div className="mt-[-5px]">
-                <span className="text-[13px]">Ahli tht</span>
-              </div>
+    return (
+        <div className="h-screen font-poppins">
+            {/* SECTION HEAD */}
+            <div className="flex h-20 ">
+                <div className="flex ">
+                    <div className="p-4">
+                        <span className="text-[26px] font-bold text-donor cursor-pointer">
+                            Donor
+                        </span>
+                        <span className="text-[26px] font-bold text-pdg cursor-pointer">
+                            .Pdg
+                        </span>
+                    </div>
+                    <div className="flex pl-[420px] ml-[90px]">
+                        <div className="w-[600px] text-right p-4">
+                            <div>
+                                <span className="font-poppins font-semibold w-[600px]">
+                                    Dr.fadel Muhammad
+                                </span>
+                            </div>
+                            <div className="mt-[-5px]">
+                                <span className="text-[13px]">Ahli tht</span>
+                            </div>
+                        </div>
+                        <div className="flex pt-0  w-[60px] h-[60px] mt-3  ">
+                            <img
+                                src={Profil}
+                                alt="Profil"
+                                className="w-full h-auto rounded-full w-[50px] h-[50px]"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-<<<<<<< HEAD
-            <div className="flex pt-0  w-[60px] h-[60px] mt-3  ">
-              <img
-                src={Profil}
-                alt="Profil"
-                className="w-full h-auto rounded-full w-[50px] h-[50px]"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* SECTION DASHBOARD */}
-      <div className="flex mt-2 ">
-        <div className="bg-white w-52 h-[400px] flex  rounded-md">
-          <nav className="text-border w-full cursor-pointer  ">
-            <ul className=" flex flex-col text-[14px] ">
-              <Link to="/DasboardAdmin">
-                <li className=" p-[25px] pt-[10px] mt-[10px] hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300 h-10 flex">
-                  <img
-                    src={frame}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px]"
-                  />
-                  Dashboard
-                </li>
-              </Link>
-
-              <li className="p-[25px] pt-[10px] h-10 text-black font-semibold text-[15px] hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300">
-                Input data
-              </li>
-              <Link to="/DaftarRsGolDarah">
-                <li className=" p-[25px] pt-[10px] h-10 bg-donor text-white  rounded-md flex">
-                  <img
-                    src={droplet}
-                    alt="frame"
-                    className="w-[20px] h-[20spx] mr-[10px]"
-                  />
-                  Gol Darah
-                </li>
-              </Link>
-              <Link to="/DaftarPersyaratan">
-                <li className="p-[25px] pt-[10px] h-10 hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300 flex">
-                  <img
-                    src={fileearmark}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px]"
-                  />
-                  Persyaratan
-                </li>
-              </Link>
-              <li className="p-[25px] pt-[10px] h-10  hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300 flex rounded-md">
-                <Link to="/DaftarArtikel" className="flex">
-                  <img
-                    src={pen}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px] "
-                  />{" "}
-                  Artikel
-                </Link>
-              </li>
-              <Link>
-                <li className="p-[25px] pt-[10px] h-10  text-black font-semibold text-[15px] hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300  flex">
-                  Informasi
-                </li>
-              </Link>
-              <Link>
-                <li className="p-[25px] pt-[10px] h-10 hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300  flex">
-                  <img
-                    src={people}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px]"
-                  />{" "}
-                  Pendonor
-                </li>
-              </Link>
-              <Link>
-                <li className="p-[25px] pt-[10px] h-10 hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300  flex">
-                  <img
-                    src={peoplemin}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px]"
-                  />{" "}
-                  Butuh Darah
-                </li>
-              </Link>
-              <Link>
-                <li className="p-[25px] pt-[10px] h-10 hover:bg-donor hover:rounded-md hover:opacity-50 hover:text-white transition-300  flex">
-                  <img
-                    src={peopleplush}
-                    alt="frame"
-                    className="w-[20px] h-[20px] mr-[10px]"
-                  />{" "}
-                  Boking Darah
-                </li>
-              </Link>
-            </ul>
-          </nav>
-        </div>
-
-        {/* SECTION HALAMAN */}
-        <div className="bg-white ml-3 rounded-md h-[700px] w-[850px] w-screen ">
-          <div className="">
-            <div className="">
-              <div className="m-6 ml-8  p-auto h-[450px]">
-                <span className="text-pdg font-bold text-[20px]">DAFTAR</span>{" "}
-                <span className="text-donor font-bold text-[20px]">
-                  RUMAH SAKIT
-                </span>
-                <form action="" className="mt-5 flex">
-                  <input
-                    type="text"
-                    placeholder="Cari Rumah Sakit"
-                    className="outline-none placeholder-border border-border border-2 rounded-md w-[600px] h-10 p-4"
-                  />
-                  <Link
-                    to="/InputGolDarah"
-                    className="flex bg-tambahartikel rounded-md ml-[180px] w-[180px] h-10 font-semibold text-white p-[8px]"
-                  >
-                    <img
-                      src={plush}
-                      alt="plush"
-                      className="m-[2px] mr-2 w-[20px] h-[20px]"
-                    />{" "}
-                    Tambah Artikel
-                  </Link>
-                </form>
-                <table className="w-full mt-10">
-                  <tr className="bg-aburam ">
-                    <td className="py-3">Gambar</td>
-                    <td>Nama/Alamat</td>
-                    <td>Tipe Gol Darah</td>
-                    <td>Jumlah Tersedia</td>
-                    <td>Aksi</td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src={rs} alt="" className="py-3" />
-                    </td>
-                    <td>
-                      <p>Semen Padang Hospital</p>
-                      <p>Jl. Kuranji, Padang Timur.</p>
-                    </td>
-                    <td className="text-center">
-                      <p>A</p>
-                      <p>B</p>
-                      <p>AB</p>
-                      <p>O</p>
-                    </td>
-                    <td className="text-center">
-                      <p>16</p>
-                      <p>5</p>
-                      <p>11</p>
-                      <p>5</p>
-                    </td>
-                    <td>
-                      <button className="flex w-[25px] ">
-                        <img src={pencil} alt="" className="" />
-                        <img src={smph} alt="" className="pl-[1px]" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src={rs2} alt="" className="py-3" />
-                    </td>
-                    <td>
-                      <p>Semen Padang Hospital</p>
-                      <p>Jl. Kuranji, Padang Timur.</p>
-                    </td>
-                    <td className="text-center">
-                      <p>A</p>
-                      <p>B</p>
-                      <p>AB</p>
-                      <p>O</p>
-                    </td>
-                    <td className="text-center">
-                      <p>16</p>
-                      <p>5</p>
-                      <p>11</p>
-                      <p>5</p>
-                    </td>
-                    <td>
-                      <button className="flex w-[25px] ">
-                        <img src={pencil} alt="" className="" />
-                        <img src={smph} alt="" className="pl-[1px]" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src={rs} alt="" className="py-3" />
-                    </td>
-                    <td>
-                      <p>Semen Padang Hospital</p>
-                      <p>Jl. Kuranji, Padang Timur.</p>
-                    </td>
-                    <td className="text-center">
-                      <p>A</p>
-                      <p>B</p>
-                      <p>AB</p>
-                      <p>O</p>
-                    </td>
-                    <td className="text-center">
-                      <p>16</p>
-                      <p>5</p>
-                      <p>11</p>
-                      <p>5</p>
-                    </td>
-                    <td>
-                      <button className="flex w-[25px] ">
-                        <img src={pencil} alt="" className="" />
-                        <img src={smph} alt="" className="pl-[1px]" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src={rs2} alt="" className="py-3" />
-                    </td>
-                    <td>
-                      <p>Semen Padang Hospital</p>
-                      <p>Jl. Kuranji, Padang Timur.</p>
-                    </td>
-                    <td className="text-center">
-                      <p>A</p>
-                      <p>B</p>
-                      <p>AB</p>
-                      <p>O</p>
-                    </td>
-                    <td className="text-center">
-                      <p>16</p>
-                      <p>5</p>
-                      <p>11</p>
-                      <p>5</p>
-                    </td>
-                    <td>
-                      <button className="flex w-[25px] ">
-                        <img src={pencil} alt="" className="" />
-                        <img src={smph} alt="" className="pl-[1px]" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img src={rs} alt="" className="py-3" />
-                    </td>
-                    <td>
-                      <p>Semen Padang Hospital</p>
-                      <p>Jl. Kuranji, Padang Timur.</p>
-                    </td>
-                    <td className="text-center">
-                      <p>A</p>
-                      <p>B</p>
-                      <p>AB</p>
-                      <p>O</p>
-                    </td>
-                    <td className="text-center">
-                      <p>16</p>
-                      <p>5</p>
-                      <p>11</p>
-                      <p>5</p>
-                    </td>
-                    <td>
-                      <button className="flex w-[25px] ">
-                        <img src={pencil} alt="" className="" />
-                        <img src={smph} alt="" className="pl-[1px]" />
-                      </button>
-                    </td>
-                  </tr>
-                </table>
-                <button className="w-[120px] h-[42px] bg-donor p-auto text-white text-[18px] rounded-md mt-[35px] mb-10">
-                  Simpan
-                </button>
-                <button className="w-[90px] h-[42px]  border-solid border-[3px] border-donor p-auto ml-5 text-donor text-[18px] rounded-md">
-                  Batal
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-=======
             {/* SECTION DASHBOARD */}
             <div className="flex mt-2 ">
                 <div className="bg-white w-52 h-[400px] flex  rounded-md">
@@ -596,7 +322,6 @@ const DaftarRsGolDarah = () => {
             </div>
         </div >
     );
->>>>>>> be2383806f1a0751a985cf0583962a3079327feb
 };
 
 export default DaftarRsGolDarah;
